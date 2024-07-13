f2021d30fe279d4a1b163a5806be1b39eac680b8
amin-da <darabniya@yahoo.com>
2024-07-13 17:05:25 +0330

    feat : add rect for each year

--- stripes.js
+++ stripes.js
@@ -6,9 +6,28 @@ function readData(file, id) {
 }
 
 function processData(datum) {
-  return datum;
+  const dataItem = {
+    year: parseFloat(datum.year) || 0.0,
+    avg: parseFloat(datum["J-D"]) || 0.0,
+  };
+
+  return dataItem;
 }
 
 function graph(data, id) {
   console.log(data, id);
+  let stripeWidth = 4;
+  let stripeHeight = 300;
+  // let land = d3.select(id);
+  // let svg = land.append("svg");
+  // svg.attr("width", stripeWidth * data.length);
+  // svg.attr("height", stripeHeight);
+
+  let svg = d3
+    .select(id)
+    .append("svg")
+    .attr("width", stripeWidth * data.length)
+    .attr("height", stripeHeight);
+
+  let stripes = svg.selectAll("rect").data(data).enter().append("rect");
 }
