3b2758e286d963e24c68781d7ba4e2285b5eace1
amin-da <darabniya@yahoo.com>
2024-07-20 11:08:58 +0330

    feat : use d3 scaleliner function for visuialization

--- stripes.js
+++ stripes.js
@@ -15,7 +15,28 @@ function processData(datum) {
 }
 
 function graph(data, id) {
-  console.log(data, id);
+  // console.log(data, id);
+  let colors = [
+    "#08306b",
+    "#08519c",
+    "#2171b5",
+    "#4292c6",
+    "#6baed6",
+    "#9ecae1",
+    "#c6dbef",
+    "#f7fbff",
+    "#deebf7",
+    "#ffffcc",
+    "#ffeda0",
+    "#fed976",
+    "#feb24c",
+    "#fd8d3c",
+    "#fc4e2a",
+    "#e31a1c",
+    "#bd0026",
+    "#800026",
+  ];
+
   let stripeWidth = 4;
   let stripeHeight = 300;
   // let land = d3.select(id);
@@ -23,11 +44,26 @@ function graph(data, id) {
   // svg.attr("width", stripeWidth * data.length);
   // svg.attr("height", stripeHeight);
 
+  const avgData = data.map((item) => item.avg);
+  let linearScaleForData = d3
+    .scaleLinear()
+    .domain([d3.min(avgData), d3.max(avgData)])
+    .range([0, colors.length - 1]);
+
   let svg = d3
     .select(id)
     .append("svg")
     .attr("width", stripeWidth * data.length)
     .attr("height", stripeHeight);
 
-  let stripes = svg.selectAll("rect").data(data).enter().append("rect");
+  let stripes = svg
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("width", stripeWidth)
+    .attr("height", stripeHeight)
+    .attr("x", (d, i) => i * stripeWidth)
+    .attr("y", 0)
+    .style("fill", (d, i) => colors[Math.round(linearScaleForData(d.avg))]);
 }
